e0049ec021f957221b5e6b6f8aaf53b4f79e99d8
agnesofdeo <agnesofdeo@gmail.com>
2023-02-05 23:24:04 +0900

    #4.2 scalar and Root Types

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 import { ApolloServer, gql } from "apollo-server"; //package.json에서    "type": "module" 를 추가해야지만 import 사용가능
 
 const typeDefs = gql`
-  type Query {
+  type User {
+    id: ID
+    username: String
+  }
+  type Tweet {
+    id: ID
     text: String
-    hello: String
+    author: User
+  }
+  type Query {
+    allTweets: [Tweet]
+    tweet(id: ID): Tweet
   }
 `;
 
