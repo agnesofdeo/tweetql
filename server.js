2a44354ed3183cf00c055b4a1278612b1e70004c
agnesofdeo <agnesofdeo@gmail.com>
2023-02-06 17:45:10 +0900

    #4.6 Query Resolvers

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const typeDefs = gql`
   type User {
     id: ID!
     username: String!
+    firstName: String!
+    lastName: String
   }
   type Tweet {
     id: ID
@@ -15,8 +17,8 @@ const typeDefs = gql`
     tweet(id: ID!): Tweet
   }
   type Mutation {
-    postTweet(text: String, userId: ID): Tweet
-    deleteTweet(id: ID): Boolean
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean
   }
 `;
 
