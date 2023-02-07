d38eb9d083828f8245760fe77ffdd5d1b0c542e5
agnesofdeo <agnesofdeo@gmail.com>
2023-02-07 17:08:24 +0900

    Modify : seerver.js 수정

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-import { ApolloServer, gql } from "apollo-server"; //package.json에서    "type": "module" 를 추가해야지만 import 사용가능
+import { ApolloServer, gql } from "apollo-server";
 import fetch from "node-fetch";
+
 let tweets = [
   {
     id: "1",
@@ -16,15 +17,19 @@ let tweets = [
 let users = [
   {
     id: "1",
-    firstName: "Nico",
+    firstName: "nico",
     lastName: "las",
   },
-  { id: "2", firstName: "Elon", lastName: "Musk" },
+  {
+    id: "2",
+    firstName: "Elon",
+    lastName: "Mask",
+  },
 ];
+
 const typeDefs = gql`
   type User {
     id: ID!
-    username: String!
     firstName: String!
     lastName: String!
     """
@@ -38,19 +43,19 @@ const typeDefs = gql`
   type Tweet {
     id: ID!
     text: String!
-    author: User!
+    author: User
   }
   type Query {
     allMovies: [Movie!]!
     allUsers: [User!]!
-    allTweets: [Tweet!]
+    allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
     movie(id: String!): Movie
   }
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet!
     """
-    Deletes a Twwt if found, else returns false
+    Deletes a Tweet if found, else returns false
     """
     deleteTweet(id: ID!): Boolean!
   }
@@ -78,12 +83,13 @@ const typeDefs = gql`
     large_cover_image: String!
   }
 `;
+// GET /api/v1/tweets
+// POST DELETE PUT /api/v1/tweets
+// GET /api/v1/tweet/:id
 
-const resorvers = {
+const resolvers = {
   Query: {
-    tweet() {
-      console.log("I'm called");
-      console.loot(root);
+    allTweets() {
       return tweets;
     },
     tweet(root, { id }) {
@@ -92,36 +98,6 @@ const resorvers = {
     allUsers() {
       console.log("allUsers called!");
       return users;
-    },
-  },
-  Mutation: {
-    postTweet(_, { text, userId }) {
-      const newTweet = {
-        id: tweets.length + 1,
-        text,
-      };
-      tweets.push(newTweet);
-      return newTweet;
-    },
-    deleteTweet(_, { id }) {
-      const tweet = tweets.find((tweet) => tweet.id === id);
-      if (!tweet) return false; // 존재하지 않는 id로 테스트 하면 false
-      tweets = tweets.filter((tweet) => tweet.id !== id);
-      return true; // 존재하는 id면 true로 받고 실제로 tweets는 정리
-    },
-  },
-  User: {
-    firstName({ firstName }) {
-      return firstName;
-    },
-    fullName() {
-      console.log("fullName called!");
-      return "hello";
-    },
-  },
-  Tweet: {
-    author({ userId }) {
-      return users.find((user) => user.id === userId);
     },
     allMovies() {
       return fetch("https://yts.mx/api/v2/list_movies.json")
@@ -134,8 +110,36 @@ const resorvers = {
         .then((json) => json.data.movie);
     },
   },
+  Mutation: {
+    postTweet(_, { text, userId }) {
+      const newTweet = {
+        id: tweets.length + 1,
+        text,
+        userId,
+      };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(_, { id }) {
+      const tweet = tweets.find((tweet) => tweet.id === id);
+      if (!tweet) return false;
+      tweets = tweets.filter((tweet) => tweet.id !== id);
+      return true;
+    },
+  },
+  User: {
+    fullName({ firstName, lastName }) {
+      return `${firstName} ${lastName}`;
+    },
+  },
+  Tweet: {
+    author({ userId }) {
+      return users.find((user) => user.id === userId);
+    },
+  },
 };
-const server = new ApolloServer({ typeDefs });
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
